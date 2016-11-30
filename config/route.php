2a5a55ce8229f6b60fed1f5681292eb72f39dfcb
DESKTOP-43EH4JU\quoc_trinh <nguyenquoctrinhctt3@gmail.com>
2016-11-30 17:08:02 +0700

    update 04

--- config/route.php
+++ config/route.php
@@ -6,30 +6,39 @@ namespace Config;
  */
 class Route
 {
-	private $_router;
+	/**
+	 * @param mixed $route
+	 */
+	public function setRoute($route)
+	{
+		$this->_route = $route;
+	}
+	/**
+	 * @return mixed
+	 */
+	public function getRoute()
+	{
+		return $this->_route;
+	}
+	private $_route;
 
 	public function __construct($router)
 	{
 
-		$this->_router = $router;
+		$this->_route = $router;
 
-		$this->_router->get("/", "IndexController@index");
+		$this->_route->get("/", "IndexController@index");
 
-		$this->_router->any("/user/registration", "UserController@registration");
-		$this->_router->any("/user/login", "UserController@login");
-		$this->_router->get("/user/logout", "UserController@logout");
-		$this->_router->get("/user/successful", "UserController@successful");
-		$this->_router->get("/user/home", "UserController@home");
-		$this->_router->any("/user/profile/{:id}", "UserController@profile");
-		$this->_router->any("/user/changeemail", "UserController@change_email");
-		$this->_router->any("/user/changepassword", "UserController@change_password");
-		$this->_router->get("/user/manage", "UserController@manage");
-		$this->_router->get("/user/confirm/{:any}", "UserController@confirm");
-		$this->_router->any("/user/search", "UserController@search");
-	}
-
-	public function getRoute()
-	{
-		return $this->_router;
+		$this->_route->any("/user/registration", "UserController@registration");
+		$this->_route->any("/user/login", "UserController@login");
+		$this->_route->get("/user/logout", "UserController@logout");
+		$this->_route->get("/user/successful", "UserController@successful");
+		$this->_route->get("/user/home", "UserController@home");
+		$this->_route->any("/user/profile/{:id}", "UserController@profile");
+		$this->_route->any("/user/changeemail", "UserController@change_email");
+		$this->_route->any("/user/changepassword", "UserController@change_password");
+		$this->_route->get("/user/manage", "UserController@manage");
+		$this->_route->get("/user/confirm/{:any}", "UserController@confirm");
+		$this->_route->any("/user/search", "UserController@search");
 	}
 }
